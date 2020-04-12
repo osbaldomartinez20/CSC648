8d02799e75702e5efdbc87b9e36d9127a6aa369b
Osbaldo Martinez Iniguez <omartin6@mail.sfsu.edu>
2020-04-11 23:50:09 -0700

    Update db.js

--- application/config/db.js
+++ application/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: "csc648-team03.cnzsqesnuc2l.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "RLa2OAk0bCqzlweqFUH1",
-  database: "csc648-team03",
+  host: "",
+  user: "",
+  password: "",
+  database: "",
   multipleStatements: true
 });
 
